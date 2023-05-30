9feb0fed02a68ae78b6ed1c0d60df3a2b5296ea5
San <San>
2023-05-30 16:17:58 -0700

    Initial commit

---
+++ src/test/java/com/payments/MainTest.java
@@ -0,0 +1,20 @@
+package com.payments;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MainTest {
+    Main main;
+
+    @Before
+    public void setUp() {
+        main = new Main();
+    }
+
+    @Test
+    public void testMain() {
+        assertTrue(true);
+    }
+}
